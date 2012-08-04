7f02999d58f83f9d368ceb169d97b46ed78be08b
Sean McCann <sean@mudbugmedia.com>
2012-08-04 15:17:28 -0400

    Fixed the indenting level of the code now that the wrapping block has been removed.

--- client/palette.js
+++ client/palette.js
@@ -1,11 +1,11 @@
-  Template.hello.greeting = function () {
-    return "Welcome to palette.";
-  };
+Template.hello.greeting = function () {
+	return "Welcome to palette.";
+};
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
+Template.hello.events = {
+	'click input' : function () {
+		// template data, if any, is available in 'this'
+		if (typeof console !== 'undefined')
+		console.log("You pressed the button");
+	}
+};
